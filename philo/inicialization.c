b8c2a76b4eb6accbe415d0153e03f1bd96606dd6
Eduardo Blazquez Rubio <edblazqu@c1r11s4.42madrid.com>
2025-12-15 14:11:53 +0100

    started arrays

--- philo/inicialization.c
+++ philo/inicialization.c
@@ -32,6 +32,9 @@ int	check_input(int ac, char **av)
 
 int	save_info(t_philo *philo, char **av, int ac)
 {
+	int	i;
+
+	i = 0;
 	philo->n_philosophers = ft_atoi(av[1]);
 	philo->time_to_die = ft_atoi(av[2]);
 	philo->time_to_eat = ft_atoi(av[3]);
@@ -43,7 +46,11 @@ int	save_info(t_philo *philo, char **av, int ac)
 	philo->threads = ft_calloc(sizeof(pthread_t), philo->n_philosophers);
 	if (!philo->threads)
 		return (0);
-	philo->table.forks = ft_calloc(sizeof(int), philo->n_philosophers);
+	philo->table.forks = ft_calloc(sizeof(bool), philo->n_philosophers);
+	if (!philo->table.forks)
+		return (0);
 	philo->table.philos = 0;
+	while (i < philo->n_philosophers)
+		philo->table.forks[i++] = false;
 	return (1);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -15,13 +15,14 @@
 
 # include <stdio.h>
 # include <stdlib.h>
+# include <stdbool.h>
 # include <pthread.h>
 # include <sys/time.h>
 
 typedef struct s_table
 {
-	int	philos;
-	int	*forks;
+	int		philos;
+	bool	*forks;
 }	t_table;
 
 typedef struct s_philo
