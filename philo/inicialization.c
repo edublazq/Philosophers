59f828b9a21a941dae625126bebe502f9c530406
Eduardo Blazquez Rubio <edblazqu@c2r12s1.42madrid.com>
2025-12-23 07:40:04 +0100

    updated

--- philo/inicialization.c
+++ philo/inicialization.c
@@ -34,7 +34,6 @@ int	save_info(t_philo *philo, char **av, int ac)
 {
 	int	i;
 
-	i = 0;
 	philo->n_philosophers = ft_atoi(av[1]);
 	philo->time_to_die = ft_atoi(av[2]);
 	philo->time_to_eat = ft_atoi(av[3]);
@@ -43,14 +42,15 @@ int	save_info(t_philo *philo, char **av, int ac)
 		philo->n_times = ft_atoi(av[5]);
 	else
 		philo->n_times = -1;
-	philo->threads = ft_calloc(sizeof(pthread_t), philo->n_philosophers);
-	if (!philo->threads)
+	philo->philos = ft_calloc(sizeof(t_each), philo->n_philos);
+	if (!philo->philos)
 		return (0);
-	philo->table.forks = ft_calloc(sizeof(bool), philo->n_philosophers);
+	philo->table.forks = ft_calloc(sizeof(pthread_mutex_t), philo->n_philos);
 	if (!philo->table.forks)
 		return (0);
 	philo->table.philos = 0;
-	while (i < philo->n_philosophers)
-		philo->table.forks[i++] = false;
+	i = 0;
+	while (i < philo->n_philos)
+		philo->philos[i++].dead = false;
 	return (1);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -21,26 +21,32 @@
 
 typedef struct s_table
 {
-	int		philos;
-	bool	*forks;
+	int				philos;
+	pthread_mutex_t	*forks;
 }	t_table;
+
+typedef struct s_each
+{
+	int		n_foods;	
+	bool	dead;
+}	t_each;
 
 typedef struct s_philo
 {
-	int			n_philosophers;
+	int			n_philos;
 	int			time_to_die;
 	int			time_to_eat;
 	int			time_to_sleep;
 	int			n_times;
-	pthread_t	*threads;
 	pthread_t	master;
 	t_table		table;
+	t_each		*philos;
 }	t_philo;
 
 int		ft_atoi(const char *nptr);
 void	ft_puterror(const char *s);
 int		check_input(int ac, char **av);
-void	save_info(t_philo *philo, char **av, int ac);
+int		save_info(t_philo *philo, char **av, int ac);
 void	*ft_calloc(size_t nmemb, size_t size);
 
 #endif
